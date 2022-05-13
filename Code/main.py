5c85fc1ec6af9e505929d485b8779bde4b5b9a90
steffen <steffen.neumann@uni-oldenburg.de>
2022-05-13 11:05:03 +0200

    Zum nutzen von Python auf dem PC muss jetzt die Libary fake-rpigpio instalirt werden. In PyCharm am Besten unter View -> Tool Window -> Python Packages gehen und dort nach fake-rpigpio suchen und dann wirklich nur das instaliren was genau so heißt. Wenn es einen Fehler gibt kann es sein das bei dir pip noch nicht instalirt ist. Unter Linux kann man das über "sudo apt install pip3" pip für python3 instaliren. Der code ist unter Verison Python 3.10 geschriben.

--- Code/main.py
+++ Code/main.py
@@ -1,3 +1,25 @@
+#Version = 0.0.1
+#Autor = Steffen Neumann
+#Programfunktion =
+
+#importe
+#Nicht ändern so lassen der code funktionirt so auf den Computer und auf den Rassperry
+try:
+    import RPi.GPIO
+except (RuntimeError, ModuleNotFoundError):
+    import fake_rpigpio.utils
+    fake_rpigpio.utils.install()
+    #GPIO.install()
+
+# GPIO zeigt auf RPi.GPIO damit man weniger schreiben muss
+GPIO = RPi.GPIO
+
+#Main
 if __name__=="__main__":
+    print("Hello world")
+    GPIO.setmode(1)
+
+
+
 
 
