9c3c01560fda0a2f2625109f0b92e66ef3de87bb
Kjell Hanken <69535642+kjell5317@users.noreply.github.com>
2022-07-15 11:53:27 +0200

    MQTT messages for Co2, Temperature & Humidity

--- Code/Test/NeoPixel.ino
+++ Code/Test/NeoPixel.ino
@@ -107,12 +107,15 @@ unsigned int lastState = 0;       // Zustand des Druckknopfes am Gehäuse
 float lastLux = 0;                  // vorheriger vom BH1750 erhaltene LUX-Wert oder 0 nach Neustart
 float lastLuxSend = 0;              // LUX-Wert der letzten Uebermittlung an MQTT-Server oder 0 nach Neustart
 unsigned long lastBrightness = 0;   // Zeitpunkt der letzten LUX-Übermittlung an MQTT-Server oder 0 nach Neustart
-
-boolean sens = false;
-
-// void IRAM_ATTR TimerHandler() {
-//  sens = true;
-// }
+// Temp
+float lastTemp = 0;
+unsigned long lastTempTime = 0;
+// Hum
+float lastHum = 0;
+unsigned long lastHumTime = 0;
+// CO2
+float lastCo2 = 0;
+unsigned long lastCo2Time = 0;
 
 //
 // Setup
@@ -122,13 +125,6 @@ void setup() {
   delay(300);
   
   Wire.begin(); // Starte I2C
-
-  // Interval in microsecs
-  // if (ITimer.attachInterruptInterval(TIMER_INTERVAL_MS * 1000, TimerHandler)) {
-  //  Serial.print("Starting ITimer OK");
-  // } else {
-  //   Serial.println("Can't set ITimer correctly. Select another freq. or interval");
-  // }
   
   pinMode(button, INPUT);          // schaltet den Pin, an den der Button angeschlossen ist, auf Eingang
   pinMode(LED_BUILTIN, OUTPUT);
@@ -329,16 +325,19 @@ void getTime() {
     hour = timeClient.getHours(); // Stunde
     Serial.print("Time: ");
     Serial.print(hour);
-    if (hour > 12) { // 12h Format
+    if (hour >= 12) { // 12h Format
       hour -= 12;
+    }
+    if (minute == 60) {
+      minute = 0;
     }
     minute = timeClient.getMinutes(); // Minute
     Serial.print(":");
     Serial.print(minute);
     Serial.println();
 
-    hourLED = map(hour, 0, 12, 0, NUM - 1); // Map Stunde zu LED
-    minuteLED = map(minute, 0, 60, 0, NUM - 1); // Map Minute zu LED
+    hourLED = map(hour, 0, 11, 0, NUM - 1); // Map Stunde zu LED
+    minuteLED = map(minute, 0, 59, 0, NUM - 1); // Map Minute zu LED
   }
   updateClock();
 }
@@ -472,14 +471,6 @@ void bh1750() {
 // BME680
 void bme680() {
   if (BME680.run()) { // fragt die Bsec-Library, ob am bme680 neue Messwerte vorliegen
-    // new value arrived
-    String output = String(BME680.iaqAccuracy);
-    output += ", " + String(BME680.temperature);
-    output += ", " + String(BME680.humidity);
-    output += ", " + String(BME680.staticIaq);
-    output += ", " + String(BME680.co2Equivalent);
-    Serial.println(output);
-
     // Temperatur
     float temp = BME680.temperature;
     int h = map(constrain(temp, 15, 27), 15, 27, 120, 0); // Map temperature to HSV scale between 240, 0
@@ -487,10 +478,20 @@ void bme680() {
           //  unterdrückt Normalwerte, zeigt nur oberes und unteres Drittel der Temperatur-Range auf strip2 an
       strip2[0] = CRGB::Black;
     } else {
-      Serial.println(h);
       strip2[0] = CHSV(h, 255, brightness);
-      FastLED.show();
       lastLED[0] = CHSV(h, 255, brightness); // Make copy
+    }
+    FastLED.show();
+    if (abs(temp - lastTemp) > 0.1 || lastTempTime == 0) {
+      if (millis() - lastTempTime > 5000 || lastTempTime == 0) {
+            // um den MQTT-Server nicht mit Nachrichten zu überschwemmen, 
+            // müssen mindestens 5 Sekunden zwischen zwei Übermittlungen liegen
+        lastTemp = temp;
+        topic = id + "/sens/temp";
+        dtostrf(temp, 3, 1, msg); // Konvertiere float zu char*
+        client.publish(topic.c_str(), msg);
+        lastTempTime = millis();
+      }
     }
     // Humidity
     float hum = BME680.humidity;
@@ -498,14 +499,43 @@ void bme680() {
     if ((80 < h && h < 90) || !light) { // if light off or h green don't show
       strip2[6] = CRGB::Black;
     } else {
-      Serial.println(h);
       strip2[6] = CHSV(h, 255, brightness);
-      FastLED.show();
       lastLED[6] = CHSV(h, 255, brightness);
+    }
+        FastLED.show();
+    if (abs(hum - lastHum) > 0.5 || lastHumTime == 0) {
+      if (millis() - lastHumTime > 5000 || lastHumTime == 0) {
+            // um den MQTT-Server nicht mit Nachrichten zu überschwemmen, 
+            // müssen mindestens 5 Sekunden zwischen zwei Übermittlungen liegen
+        lastHum = hum;
+        topic = id + "/sens/hum";
+        dtostrf(hum, 3, 1, msg); // Konvertiere float zu char*
+        client.publish(topic.c_str(), msg);
+        lastHumTime = millis();
+      }
     }
     // IAQ
     if (BME680.iaqAccuracy > 0) { // IAQ Accuracy above 0 to get safe values
-      
+      float co2 = BME680.co2Equivalent;
+      if (co2 < 600 || !light) {
+        strip2[2] = CRGB::Black;
+      } else {
+        h = map(co2, 600, 750, 80, 0);
+        strip2[2] = CHSV(h, 255, brightness);
+        lastLED[2] = CHSV(h, 255, brightness);
+      }
+      FastLED.show();
+      if (abs(co2 - lastCo2) > 3 || lastCo2Time == 0) {
+        if (millis() - lastCo2Time > 5000 || lastCo2Time == 0) {
+              // um den MQTT-Server nicht mit Nachrichten zu überschwemmen, 
+              // müssen mindestens 5 Sekunden zwischen zwei Übermittlungen liegen
+          lastCo2 = co2;
+          topic = id + "/sens/co2";
+          dtostrf(co2, 3, 1, msg); // Konvertiere float zu char*
+          client.publish(topic.c_str(), msg);
+          lastCo2Time = millis();
+        }
+      }
     }
   } else if (BME680.status != BSEC_OK || BME680.bme680Status != BME680_OK) { // prueft ob Fehlersituation vorliegt
     Serial.print("FAIL ");
@@ -513,7 +543,6 @@ void bme680() {
     Serial.print(" : ");
     Serial.println(BME680.bme680Status);
   }
-  sens = false;
 }
 
 // Loop
@@ -523,10 +552,8 @@ void loop() {
   }
   client.loop();
   checkButton();
-  //if (sens) {
-   // bh1750();
-    bme680();
-  //}
+  bh1750();
+  bme680();
   if (pomodoro) { // Timer
     updateTimer();
   } else {
